e1c0cd34edd22333dd7c99c76c5e6254c4dc15b6
danah2139 <danah2139@gmail.com>
2019-11-11 00:46:45 +0200

    finish auth flow

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    cookieKey: process.env.COOKIE_KEY,
+    mongoURI:process.env.MONGO_URI
+    
+    
+
+};
+
+//'mongodb+srv://myuser:iEWTX7f22q9HlGPe@email-prod-wgeyf.mongodb.net/prod?retryWrites=true&w=majority'
--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,40 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //const FacebookStrategy = require('passport-facebook').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys.js');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user,done)=> {
+    done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id).then(user => {
+        done(null,user);
+    })
+});
 passport.use(
     new GoogleStrategy({
-        clientID:keys.googleClientID,
-        clientSecret:keys.googleClientSecret,
-        callbackURL:'/auth/google/callback'
-    },(accessToken,refreshToken,profile,done)=>{
-        console.log('aceess token',accessToken);
-        console.log('refresh token',refreshToken);
-        console.log('profile:',profile);
-    })
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id }).then(existingUser => {
+                if(existingUser){
+                    done(null,existingUser);
+                }else{
+                    new User({ googleId: profile.id })
+                    .save()
+                    .then(user =>done(null,user));// check if save to db
+                }
+            });
+            
+            
+        }
+           
+    )
 );
 //passport.use(new FacebookStrategy());
