609d20c810302aa19b6d514ee46406aa5847bb8d
danah2139 <danah2139@gmail.com>
2019-11-17 11:57:11 +0200

    Added billing and client side app

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SurveyNew = () => {
+    return(<div>
+        SurveyNew
+    </div>);
+}; 
+export default SurveyNew; 
--- config/prod.js
+++ config/prod.js
@@ -2,10 +2,11 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
-    mongoURI:process.env.MONGO_URI
+    mongoURI:process.env.MONGO_URI,
+    stripePublishableKey:process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey:process.env.STRIPE_SECRET_KEY
     
     
 
 };
 
-//'mongodb+srv://myuser:iEWTX7f22q9HlGPe@email-prod-wgeyf.mongodb.net/prod?retryWrites=true&w=majority'
