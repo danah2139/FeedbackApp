8858b5388864294e6baba432979435192608e8fb
danah2139 <danah2139@gmail.com>
2019-11-18 23:51:20 +0200

    Add list of surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import {FETCH_USER} from './types';
+import {FETCH_USER,FETCH_SURVEYS} from './types';
+import { async } from 'q';
 
 
 export const fetchUser = () => async dispatch => {
@@ -18,5 +19,12 @@ export const submitSurvey = (values,history) => async dispatch =>{
     const res = await axios.post('/api/surveys',values);
     history.push('/surveys');
     dispatch({type:FETCH_USER ,payload:res.data});
+
+};
+
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+    dispatch({type:FETCH_SURVEYS ,payload:res.data});
+
 
 };
---
+++ client/src/reducers/surveysReducer.js
@@ -0,0 +1,12 @@
+import {FETCH_SURVEYS} from '../actions/types';
+import { stat } from 'fs';
+
+export default function(state=[],action){
+    switch(action.type){
+        case FETCH_SURVEYS:
+            return action.payload;
+        default:
+            return state;
+    }
+
+}
